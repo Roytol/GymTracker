bca5c13f0fb3cd9e93acda8291ed6c52ee80bddf
Roytol <roey.toledo@gmail.com>
2025-12-03 15:07:04 +0200

    update

--- src/lib/supabase-middleware.ts
+++ src/lib/supabase-middleware.ts
@@ -24,6 +24,11 @@ export async function updateSession(request: NextRequest) {
                     )
                 },
             },
+            cookieOptions: {
+                maxAge: 60 * 60 * 24 * 365, // 1 year
+                sameSite: 'lax',
+                secure: process.env.NODE_ENV === 'production',
+            },
         }
     )
 
